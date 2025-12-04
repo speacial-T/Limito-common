eeda70b462d40186bdb58c2d9d50aba273714b28
gosera <gosera0204@gmail.com>
2025-12-05 01:44:27 +0900

    feat: ErrorCode 기반 of() 메서드 추가

--- src/main/java/com/limito/common/exception/AppException.java
+++ src/main/java/com/limito/common/exception/AppException.java
@@ -2,6 +2,8 @@ package com.limito.common.exception;
 
 import org.springframework.http.HttpStatus;
 
+import com.limito.common.code.ErrorCode;
+
 import lombok.Getter;
 
 @Getter
@@ -9,9 +11,18 @@ public class AppException extends RuntimeException {
 
 	private final HttpStatus status;
 
+	public AppException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.status = errorCode.getStatus();
+	}
+
 	public AppException(HttpStatus status, String message) {
 		super(message);
 		this.status = status;
+	}
+
+	public static AppException of(ErrorCode errorCode) {
+		return new AppException(errorCode);
 	}
 
 	public static AppException of(HttpStatus status, String message) {
