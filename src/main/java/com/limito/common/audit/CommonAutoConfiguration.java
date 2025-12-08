0a52beebadd827cd39fff02a741599e590561391
gosera <gosera0204@gmail.com>
2025-12-08 11:59:53 +0900

    feat(api-gateway): 불필요한 주석 삭제

--- src/main/java/com/limito/common/audit/CommonAutoConfiguration.java
+++ src/main/java/com/limito/common/audit/CommonAutoConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.Ordered;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @AutoConfiguration
+@ComponentScan("com.limito.common")
 @EnableJpaAuditing
 public class CommonAutoConfiguration {
 
