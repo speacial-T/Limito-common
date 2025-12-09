eae28c83cecb9a2dd2de4de0227aa09d79e0bacb
gosera <gosera0204@gmail.com>
2025-12-09 10:57:09 +0900

    refactor(common): 코드 리펙토링

--- src/main/java/com/limito/common/audit/UserContextHolder.java
+++ src/main/java/com/limito/common/audit/UserContextHolder.java
@@ -6,27 +6,12 @@ public final class UserContextHolder {
 
 	private static final ThreadLocal<UserContext> CONTEXT = new ThreadLocal<>();
 
-	private UserContextHolder() {
-	}
-
 	public static void set(UserContext userContext) {
 		CONTEXT.set(userContext);
 	}
 
-	public static UserContext get() {
-		return CONTEXT.get();
-	}
-
-	public static Optional<UserContext> getOptional() {
-		return Optional.ofNullable(CONTEXT.get());
-	}
-
 	public static Optional<Long> getCurrentUserId() {
 		return Optional.ofNullable(CONTEXT.get()).map(UserContext::getUserId);
-	}
-
-	public static Optional<String> getCurrentUserEmail() {
-		return Optional.ofNullable(CONTEXT.get()).map(UserContext::getEmail);
 	}
 
 	public static Optional<UserRole> getCurrentUserRole() {
